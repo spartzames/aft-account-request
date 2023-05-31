efca8abe6ab002f6c3d683341d2fea19602041ab
spartzames <spartzames@gmail.com>
2023-06-01 00:11:43 +0900

    fix

--- terraform/account-request.tf
+++ terraform/account-request.tf
@@ -1,3 +1,17 @@
+provider "aws" {
+  profile = "default"
+  region = var.ct_home_region
+}
+
+variable "ct_home_region" {
+  description = "The region from which this module will be executed. This MUST be the same region as Control Tower is deployed."
+  type        = string
+  validation {
+    condition     = can(regex("(us(-gov)?|ap|ca|cn|eu|sa|me|af)-(central|(north|south)?(east|west)?)-\\d", var.ct_home_region))
+    error_message = "Variable var: region is not valid."
+  }
+}
+
 module "req_accnt_dev01" {
   source = "./modules/aft-account-request"
 
